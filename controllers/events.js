cbc4b1e456345f7741b5b3636c37643f7bc68794
Paul Cortes <paul.cortesr@perficient.com>
2022-01-25 05:52:41 -0500

    DELETE eventos

--- controllers/events.js
+++ controllers/events.js
@@ -54,7 +54,7 @@ const actualizarEvento = async (req, res = response) => {
     if (evento.user.toString() !== uid) {
       return res.status(401).json({
         ok: false,
-        msg: "No tiene privilegio de edutar este evento",
+        msg: "No tiene privilegio de editar este evento",
       });
     }
 
@@ -81,17 +81,45 @@ const actualizarEvento = async (req, res = response) => {
       msg: "Hable con el administrador",
     });
   }
-  return res.json({
-    ok: true,
-    eventoId,
-  });
 };
 
-const eliminarEvento = (req, res = response) => {
-  return res.json({
-    ok: true,
-    msg: "Borrar Evento",
-  });
+const eliminarEvento = async (req, res = response) => {
+  const eventoId = req.params.id;
+  const { uid } = req;
+
+	console.log(`eventoId: ${eventoId} - uid: ${uid}`);
+
+  try {
+    const evento = await Evento.findById(eventoId);
+
+    if (!evento) {
+      return res.status(404).json({
+        ok: false,
+        msg: `Evento no existe por ese id: ${eventoId}`,
+      });
+    }
+
+    if (evento.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No tiene privilegio de eliminar este evento",
+      });
+    }
+
+    await Evento.findByIdAndDelete(eventoId);
+
+
+    return res.json({
+      ok: true,
+      msg:"Evento eliminado satisfactoriamente"
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 module.exports = {
